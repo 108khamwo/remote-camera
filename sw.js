3e6c62b3448347c5d26eb0ea23e054501d4353ff
108khamwo <108khamwo@gmail.com>
2026-08-21 15:44:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='remote-camera-v01118';
+const CACHE='remote-camera-v01120';
 const STATIC_ASSETS=['./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC_ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
